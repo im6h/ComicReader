fe6dc2e660aa2f3c55b4b3b61528ccbca92bc833
Vu <35485655+tjjone98@users.noreply.github.com>
2019-02-12 09:12:00 +0700

    First Comit
    - Create project and import library
    - Create splash screen

---
+++ app/src/main/java/com/vu/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vu.comicreader.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
